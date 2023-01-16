45c1c8937d54d14a3045d73984d2972dc39dc0bb
Mateusz Lacek <mlacek19@comcast.net>
2023-01-16 09:58:58 -0600

    add for loop that starts from end of string and adds each character while incrementing in reverse to result.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+    chars = string.split('');
+    result = ''
+    for (let i = string.length - 1; i >= 0; i--) {
+        result += string[i];
+    }
+    return result
 };
 
 // Do not edit below this line
