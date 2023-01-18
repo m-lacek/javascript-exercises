64cd8bd066759a844b5ebcc3141fdaa8bba80d42
Mateusz Lacek <mlacek19@comcast.net>
2023-01-18 10:46:27 -0600

    Create condition to check which number is larger. Loop from smaller to larger and add each num to sum

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+    let larger = num1;
+    let smaller = num2;
+    let sum = 0;
+    if (num2 > num1) {
+        larger = num2
+        smaller = num1
+    }
+    for (i = smaller; i <= larger; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
