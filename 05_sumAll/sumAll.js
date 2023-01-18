3b70a36e3def8ea9f4c4000cf3b0b1b5fb24dfb4
Mateusz Lacek <mlacek19@comcast.net>
2023-01-18 10:52:53 -0600

    add condition for checking if num1 or num2 is an integer.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,6 +2,9 @@ const sumAll = function (num1, num2) {
     if (num1 < 0 || num2 < 0) {
         return "ERROR"
     }
+    if (Number.isInteger(num1) == false || Number.isInteger(num2) == false) {
+        return "ERROR"
+    }
     let larger = num1;
     let smaller = num2;
     let sum = 0;
